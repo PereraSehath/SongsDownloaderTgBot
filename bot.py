76d887ca95afac6de1d82d1ac69d21c2f8bb53f1
Sehath Perera <93459498+PereraSehath@users.noreply.github.com>
2021-12-13 23:09:07 +0530

    Annie - Eliza🌈😍
    Update bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "6"))
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    bot = Client(
+        "SongsDownloaderTgBot",
+        bot_token=BOT_TOKEN,
+        api_hash=API_HASH,
+        api_id=API_ID,
+        plugins=plugins
+    )
+    bot.run()
+© 2021 GitHub, Inc.
